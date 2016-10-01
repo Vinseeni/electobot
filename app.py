79ec8b55df573a111249d985944c450d87dea8cb
vinSeeni <srevinod@u.nus.edu>
2016-09-30 17:56:05 -0700

    removed links for opening message

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def webhook():
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                         message_text = messaging_event["message"]["text"]  # the message's text
-                        send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
+                        # send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
 
 
                 # if messaging_event.get("message"):
