e307f39009facf5390d54d6a24d0679e59b234e5
vinSeeni <srevinod@u.nus.edu>
2016-09-30 17:54:37 -0700

    more automated messages

--- app.py
+++ app.py
@@ -51,20 +51,20 @@ def webhook():
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                         message_text = messaging_event["message"]["text"]  # the message's text
                         send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
-                    # elif message_text == "anything":
-                    # send_message(sender_id, "hillary not too bad ")
-                    # sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    # recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    # message_text = messaging_event["message"]["text"]  # the message's text
-                    # send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
-                    # else:
-                    # send_message(sender_id, "you should really vote hillary ")
-                    # sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    # recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    # message_text = messaging_event["message"]["text"]  # the message's text
-                    # send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
+                    elif message_text == "undecided":
+                        send_message(sender_id, "then trust me and vote hillary")
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                        send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
+                    else:
+                        send_message(sender_id, "Wassup! Ready for election news? trump, hillary or undecided?")
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                        send_message(sender_id, "http://www.latimes.com/nation/politics/trailguide/la-na-live-updates-trailguide-hillary-clinton-pounces-on-donald-1475266902-htmlstory.html")
 
-                    
+
                 # if messaging_event.get("message"):
                 #     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                 #     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
